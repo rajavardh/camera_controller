234ea7dab0e710a8199e8c62a82d718ac4eac0e5
Rajavardhan <106825601+rajavardh@users.noreply.github.com>
2026-06-25 15:59:01 +0530

    Update dvp_sequencer.sv

--- NCC_AI/Camera_verification/dvp_tb/dvp_agent/dvp_sequencer.sv
+++ NCC_AI/Camera_verification/dvp_tb/dvp_agent/dvp_sequencer.sv
@@ -2,11 +2,8 @@
 `define DVP_SEQUENCER_SV
 
 class dvp_sequencer extends uvm_sequencer #(dvp_seq_item);
-  
-  // Register with the UVM Factory
   `uvm_component_utils(dvp_sequencer)
 
-  // Standard constructor
   function new(string name = "dvp_sequencer", uvm_component parent = null);
     super.new(name, parent);
   endfunction
