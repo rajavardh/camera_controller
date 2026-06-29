a15b6d506383d0be9db86bf13c94bb97910a3dae
Rajavardhan <106825601+rajavardh@users.noreply.github.com>
2026-06-29 15:34:24 +0530

    Update camera_vsequencer.sv

--- NCC_AI/Camera_verification/dvp_tb/camera_vsequencer.sv
+++ NCC_AI/Camera_verification/dvp_tb/camera_vsequencer.sv
@@ -4,7 +4,7 @@
 class camera_vsequencer extends uvm_sequencer;
     `uvm_component_utils(camera_ip_vsequencer)
 
-    apb_virtual_sequencer apb_vsqr; // Handle to the VIP's 
+    apb_master_vsequencerr apb_vsqr; // Handle to the VIP's 
     dvp_sequencer         dvp_seqr; 
 
 //    axi_sequencer         axi_seqr; 
